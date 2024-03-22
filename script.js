c6c34771522fd599eb9bd1c8ac9b8892c5df0423
Chuppychip <119408425+Chuppychip@users.noreply.github.com>
2024-03-22 08:46:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const ball = document.getElementById('ball');
     const goal = document.getElementById('goal');
     const gameContainer = document.getElementById('game-container');
+    const gameContainerRect = gameContainer.getBoundingClientRect();
 
     // Beweging van de bal
     document.addEventListener('keydown', function(event) {
@@ -12,16 +13,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
         switch (key) {
             case 'ArrowUp':
-                ball.style.top = (ballTop - 10) + 'px';
+                if (ballTop > gameContainerRect.top) {
+                    ball.style.top = (ballTop - 10) + 'px';
+                } else {
+                    alert('GVD, jij bent KAKNER slecht in dit spel. Da hell..');
+                }
                 break;
             case 'ArrowDown':
-                ball.style.top = (ballTop + 10) + 'px';
+                if (ballTop < gameContainerRect.bottom - ball.offsetHeight) {
+                    ball.style.top = (ballTop + 10) + 'px';
+                } else {
+                    alert('GVD, jij bent KAKNER slecht in dit spel. Da hell..');
+                }
                 break;
             case 'ArrowLeft':
-                ball.style.left = (ballLeft - 10) + 'px';
+                if (ballLeft > gameContainerRect.left) {
+                    ball.style.left = (ballLeft - 10) + 'px';
+                } else {
+                    alert('GVD, jij bent KAKNER slecht in dit spel. Da hell..');
+                }
                 break;
             case 'ArrowRight':
-                ball.style.left = (ballLeft + 10) + 'px';
+                if (ballLeft < gameContainerRect.right - ball.offsetWidth) {
+                    ball.style.left = (ballLeft + 10) + 'px';
+                } else {
+                    alert('GVD, jij bent KAKNER slecht in dit spel. Da hell..');
+                }
                 break;
         }
         // Controleer winvoorwaarde
@@ -40,3 +57,14 @@ document.addEventListener("DOMContentLoaded", function() {
                  ballRect.top > goalRect.bottom);
     }
 });
+
+#ball {
+    width: 50px;
+    height: 50px;
+    background-color: red;
+    clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
